a39fab7027c9a792992719e2f37d611855759acc
Antoine Bollengier <44288655+antoinebollengier@users.noreply.github.com>
2021-10-21 09:52:25 +0200

    Major change

--- search.js
+++ search.js
@@ -1,13 +1,13 @@
 const { exec } = require("child_process");
 const fs = require('fs')
-var singers = ''; //artist here
-var songname = ''; //songname here
+const args = process.argv.slice(2)
+var singers = args[0];
+var songname = args[1];
 
 function get_lyrics (singers, songname){
     var split_artists = singers.split(", "); // Separate differents artists : for Riton, Kah-Lo it will search for Riton, Kah-Lo - Riton - Kah-Lo
 split_artists.splice(2, 0, singers);
 for (let i = 0; i < split_artists.length; i++) {
-    console.log(split_artists[i]);
     exec("python3 searcher.py '"+split_artists[i]+"' '"+songname+"' '"+singers+"'", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
